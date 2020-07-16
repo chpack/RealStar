ac06ed3a66a8d20002f2a21d840263b1c393bc2e
yuqi chen <chenyuqide@outlook.com>
2020-07-16 22:49:33 +0800

    More structure

--- app/src/main/java/com/example/realstar/Sky.kt
+++ app/src/main/java/com/example/realstar/Sky.kt
@@ -2,26 +2,35 @@ package com.example.realstar
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
-import kotlin.Float
 
 object SkyAttr {
-    var length = 100
+    var length = 200
     var size = 100
+        set(value) {
+            field = value
+            sizeChange()
+        }
+    var sizeChange: () -> Unit = {}
 }
 
 @SuppressLint("ClickableViewAccessibility")
 class Sky(context: Context, wm: WindowManager) {
 
+    /**
+     * Views
+     * @root is the root layout, include all other views
+     * @stars is the all stars, each one is image view, to show icons
+     * @pointer is the touch position
+     */
     var root: ConstraintLayout =
         ConstraintLayout.inflate(context, R.layout.sky_layout, null) as ConstraintLayout
-    var stars = arrayOf(
+    var stars: Array<ImageView> = arrayOf(
         root.findViewById<ImageView>(R.id.star_0),
         root.findViewById<ImageView>(R.id.star_1),
         root.findViewById<ImageView>(R.id.star_2),
@@ -30,92 +39,119 @@ class Sky(context: Context, wm: WindowManager) {
         root.findViewById<ImageView>(R.id.star_5),
         root.findViewById<ImageView>(R.id.star_6)
     )
+    var pointer: View = root.findViewById<View>(R.id.pointer)
 
+    /**
+     * UserMode mean operation mode.
+     * @activity is mean normal launcher
+     * @float is use float window
+     */
     enum class UserMode { ACTIVITY, FLOAT }
 
-    var usermode = UserMode.FLOAT
+    private var usermode = UserMode.FLOAT
 
-    var startLP = ConstraintSet()
-    var midLPs = arrayOf(ConstraintSet(), ConstraintSet())
-    var currLP: ConstraintSet
-        get() = midLPs[indexLP % 2]
-        set(value) {
-            midLPs[indexLP % 2] = value
-        }
+    /**
+     * Layout of all sectuin
+     * @startLP is the first layout
+     */
+    var indexLP = 0
+    private var swapLP = arrayOf(ConstraintSet(), ConstraintSet(), ConstraintSet())
 
-    var nextLP: ConstraintSet
-        get() = midLPs[(indexLP + 1) % 2]
-        set(value) {
-            midLPs[(indexLP + 1) % 2] = value
-        }
+    private val startLP: ConstraintSet get() = swapLP[0]
+    private val currLP: ConstraintSet get() = swapLP[1 + indexLP % 2]
+    private val nextLP: ConstraintSet get() = swapLP[1 + (indexLP + 1) % 2]
 
-    var indexLP = 0;
-
-    var pointer = root.findViewById<View>(R.id.pointer)
 
     init {
-        midLPs[0].clone(root)
-        midLPs[1].clone(root)
-        startLP.clone(root)
-        startLP.constrainCircle(stars[0].id,pointer.id,0,0f)
+        initWindow()
+        initLPs()
+        root.setOnTouchListener { v, event ->
+            if (v == null || event == null) false
+            else rootListener(v, event)
+        }
+
+        SkyAttr.sizeChange = { setSize() }
+    }
+
+    /**
+     * init LayoutParams
+     */
+    private fun initLPs() {
+        swapLP.forEach { cs -> cs.clone(root) }
+
+        startLP.constrainCircle(stars[0].id, pointer.id, 0, 0f)
         stars.forEach { v ->
             if (v != stars[0])
                 startLP.constrainCircle(v.id, stars[0].id, 0, 0f)
         }
-        startLP.constrainCircle(stars[0].id, pointer.id, 0, 0f)
-        root.setOnTouchListener { v, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    pointer.x = event.rawX
-                    pointer.y = event.rawY
+    }
 
-                    startLP.applyTo(root)
-//                    stars[0].x = event.rawX - SkyAttr.size/2
-//                    stars[0].y = event.rawY - SkyAttr.size/2
-                    true
-                }
-                MotionEvent.ACTION_MOVE -> {
-                    pointer.x = event.rawX
-                    pointer.y = event.rawY
-
-                    setCenter(stars[0], event.rawX, event.rawY)
-                    indexLP++
-                    currLP.applyTo(root)
-                    true
-
-                }
-                MotionEvent.ACTION_UP -> {
-                    true
-                }
-                else -> {
-                    false
-                }
+    val rootListener: (v: View, event: MotionEvent) -> Boolean = { v, event ->
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                setCenter(event.rawX, event.rawY)
+                startWindow(event)
+                startSkyLine(event)
+                true
             }
+            MotionEvent.ACTION_MOVE -> {
+                linkStars(event)
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                doAction()
+                endWindow()
+                endSkyLine()
+                true
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                endWindow()
+                endSkyLine()
+                true
+            }
+            else -> false
         }
     }
 
-
-    fun setCenter(c: ImageView, x: Float, y: Float) {
-        setSize()
-        SkyAttr.size+=10
+    private fun setCenter(x: Float, y: Float, c: ImageView = stars[0]) {
         currLP.constrainCircle(c.id, pointer.id, 0, 0f)
-        currLP.connect(pointer.id,ConstraintSet.LEFT,root.id, ConstraintSet.LEFT, x.toInt())
-        currLP.connect(pointer.id,ConstraintSet.TOP,root.id, ConstraintSet.TOP, y.toInt())
-//        Log.d("asdfasdf", "${c.x} ${c.y} ${pointer.x} ${pointer.y}")
+        nextLP.setMargin(pointer.id, ConstraintSet.LEFT, x.toInt())
+        nextLP.setMargin(pointer.id, ConstraintSet.TOP, y.toInt())
         stars.forEachIndexed { i, v ->
             if (c != v)
                 nextLP.constrainCircle(v.id, c.id, SkyAttr.length, i * 60f)
         }
     }
 
-    fun setSize(){
-        stars.forEach { v->
-            currLP.constrainWidth(v.id,SkyAttr.size)
-            startLP.constrainWidth(v.id,SkyAttr.size)
-            nextLP.constrainWidth(v.id,SkyAttr.size)
-            currLP.constrainHeight(v.id,SkyAttr.size)
-            startLP.constrainHeight(v.id,SkyAttr.size)
-            nextLP.constrainHeight(v.id,SkyAttr.size)
+    private fun initWindow() {}
+
+
+    private fun startWindow(event: MotionEvent) {
+        if (usermode == UserMode.ACTIVITY) return
+    }
+
+    private fun startSkyLine(event: MotionEvent) {
+        startLP.applyTo(root)
+    }
+
+    private fun linkStars(event: MotionEvent) {
+        setCenter(event.rawX, event.rawY)
+        indexLP++
+        currLP.applyTo(root)
+    }
+
+    private fun doAction() {}
+
+    private fun endWindow() {}
+
+    private fun endSkyLine() {}
+
+    private fun setSize() {
+        stars.forEach { v ->
+            swapLP.forEach { lp ->
+                lp.constrainWidth(v.id, SkyAttr.size)
+                lp.constrainHeight(v.id, SkyAttr.size)
+            }
         }
     }
 }
