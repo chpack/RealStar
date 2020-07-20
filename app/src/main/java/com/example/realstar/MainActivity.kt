52c10095a44679868284c879a52b5bebc027154a
yuqi chen <chenyuqide@outlook.com>
2020-07-20 18:16:14 +0800

    Splite a AppListActivity

--- app/src/main/java/com/example/realstar/MainActivity.kt
+++ app/src/main/java/com/example/realstar/MainActivity.kt
@@ -2,25 +2,12 @@ package com.example.realstar
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_list_item_layout.view.*
 
 lateinit var sa: SkyAttr
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var sky: Sky
-    lateinit var adapter: AppListAdapter
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,93 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         sa = (application as SkyApp).sa
-//        sky = Sky(this, windowManager)
-
-        showbut.setOnClickListener {
-            if (Settings.canDrawOverlays(this)) {
-//                showFloat()
-            } else {
-                getPermis()
-            }
-        }
-
         startService(Intent(baseContext, SkyBack::class.java))
-
-        sa.actions = ActionManager(this)
-
-        adapter = AppListAdapter()
-        lists.layoutManager = LinearLayoutManager(this)
-        lists.adapter = adapter
-
-        show_launcher.setOnClickListener {
-            adapter.type = EndAction.Type.APP
-            adapter.notifyDataSetChanged()
-        }
-        show_all.setOnClickListener {
-            adapter.type = EndAction.Type.ACT
-            adapter.notifyDataSetChanged()
-        }
-    }
-
-
-    private fun getPermis() {
-        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-        intent.data = Uri.parse("package:$packageName")
-        startActivityForResult(intent, 1)
     }
 }
 
-class ViewH(root: ConstraintLayout) : RecyclerView.ViewHolder(root) {
-    private var icon = root.item_icon
-    private var title = root.item_title
-    private var line = root.item_gesture
-    private lateinit var action: EndAction
-    private var assign = root.item_assign
-    private var info = root.item_info
-
-    init {
-        root.item_assign.setOnClickListener {
-            if (action.line.isEmpty()) {
-                sa.actions.readToAssign = action
-                Log.d("asdfasdf", "on click")
-            } else {
-                sa.actions.delete(action)
-            }
-        }
-        icon.setOnClickListener {
-            sa.actions.launchApp(action)
-        }
-    }
-
-    fun reset(action: EndAction) {
-        this.action = action
-        icon.setImageDrawable(action.drawable)
-        title.text = action.title
-        line.text = action.line
-        info.text = action.name.removePrefix(action.pack)
-        if (action.line.isEmpty())
-            assign.setImageResource(android.R.drawable.ic_input_add)
-        else
-            assign.setImageResource(android.R.drawable.ic_delete)
-    }
-}
-
-class AppListAdapter() : RecyclerView.Adapter<ViewH>() {
-    var type = EndAction.Type.APP
-
-    var lists = HashMap<EndAction.Type, List<EndAction>>().apply {
-        EndAction.Type.values()
-            .forEach { type -> put(type, sa.actions.actions.filter { it.type == type }) }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewH =
-        ViewH(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.app_list_item_layout, parent, false) as ConstraintLayout
-        )
-
-    override fun getItemCount(): Int = lists[type]!!.size
-
-    override fun onBindViewHolder(holder: ViewH, position: Int) =
-        holder.reset(lists[type]!![position])
-}
--- app/src/main/java/com/example/realstar/SkyBack.kt
+++ app/src/main/java/com/example/realstar/SkyBack.kt
@@ -1,18 +1,42 @@
 package com.example.realstar
 
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Icon
 import android.os.IBinder
 import android.view.WindowManager
 
 class SkyBack : Service() {
-    lateinit var sky: Sky
+    private lateinit var sky: Sky
     override fun onCreate() {
         super.onCreate()
         val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
         sa = (application as SkyApp).sa
+        sa.actions = ActionManager(this)
         sky = Sky(this, wm)
+
+        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        nm.createNotificationChannel(
+            NotificationChannel("asdf", "asdfasdf", NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = "test text"
+            }
+        )
+
+        val pi: PendingIntent = Intent(this, MainActivity::class.java).let {
+            PendingIntent.getActivity(this, 0, it, 0)
+
+        }
+        val notification = Notification.Builder(this, "asdf")
+            .setContentTitle("Keep alive")
+            .setContentText("Kepp alive")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setLargeIcon(Icon.createWithResource(this, R.drawable.ic_launcher_background))
+            .setContentIntent(pi)
+            .setTicker("Keep alive")
+            .build()
+
+        startForeground(1, notification)
     }
 
     override fun onBind(intent: Intent?): IBinder? {
